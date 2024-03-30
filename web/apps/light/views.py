1f7383115634406135a6bd04f28b257e7ec442c8
vallenov <a2lenov@mail.ru>
2024-03-30 19:48:04 +0300

    add main led methods

---
+++ web/apps/light/views.py
@@ -0,0 +1,54 @@
+import logging
+
+from rest_framework.views import APIView
+from django.shortcuts import render
+
+from apps.utils.esp8266.esp import esp
+
+logger = logging.getLogger(__name__)
+
+
+class MainLEDPower(APIView):
+
+    def get(self, request):
+        esp.main_led.power_switch()
+        context = {
+            'power': True if esp.main_led.power else False
+        }
+        return render(request, template_name='main.html', context=context)
+
+
+class MainLEDColor(APIView):
+
+    def get(self, request):
+        val = request.GET.get('id')
+        if val:
+            esp.main_led.color_switch(val=val)
+        return render(request, template_name='main.html')
+
+
+class MainLEDBrightness(APIView):
+
+    def get(self, request):
+        val = request.GET.get('id')
+        if val:
+            esp.main_led.brightness_switch(val=val)
+        return render(request, template_name='main.html')
+
+
+class MainLEDEffect(APIView):
+
+    def get(self, request):
+        val = request.GET.get('id')
+        if val:
+            esp.main_led.effect_switch(val=val)
+        return render(request, template_name='main.html')
+
+
+class MainLEDEffectSpeed(APIView):
+
+    def get(self, request):
+        val = request.GET.get('id')
+        if val:
+            esp.main_led.effect_speed_switch(val=val)
+        return render(request, template_name='main.html')
